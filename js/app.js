13e6293a96b4650a02ac22b39904dbd42181bee3
Elena Nedkova <lenuza@gmail.com>
2020-04-30 12:59:11 +0300

    added the sections titles to the list items

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-var sections = document.getElementsByClassName('section-content');
+var sections = document.getElementsByClassName('section-title');
 var unorderedList = document.getElementById('navbar-list');
 var listItem;
 
 for(let i = 0; i < sections.length; i++) {
+   var title = sections[i].innerHTML;
    listItem = unorderedList.appendChild(document.createElement('li'));
-   listItem.innerHTML = `Item ${i+1}`;
+   listItem.innerHTML = title;
 }
